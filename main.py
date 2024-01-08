1163a075e12fec25d3767c073c87656dce7d3704
ArtursBubovics <artursbubovocs@gmail.com>
2024-01-08 17:49:07 +0200

    Version 3

--- main.py
+++ main.py
@@ -23,6 +23,16 @@ worksheet = workbook.active
 
 # Проход по каждой строке в Excel
 for index, row in df.iterrows():
+
+    # # Проверка наличия значений в текущей строке и следующей строке
+    # if pd.isna(df.loc[index, 'words']) and pd.isna(df.loc[index, 'перевод']) and pd.isna(df.loc[index + 1, 'words']) and pd.isna(df.loc[index + 1, 'перевод']):
+    #     # Ваш код, который выполнится только в случае, если оба поля в текущей и следующей строках равны NaN
+    #     print("Дальше нету полей для ввода")
+    #     break
+    # else:
+    #     # Ваш код, который выполнится, если хотя бы одно из полей в текущей или следующей строках не равно NaN
+    #     print("Поле {index} имеет не введенное значение")
+
     
 
   # Проверка цвета заливки ячейки в колонке 'В картачках'
@@ -30,6 +40,13 @@ for index, row in df.iterrows():
 
     # Если цвет заливки не белый, пропустить обработку строки
     if cell_color == '00000000':
+
+        # # Проверка на наличие NaN в полях
+        # if pd.isna(row['words']) or pd.isna(row['перевод']):
+        #     print(f"Пропуск строки {index + 2} из-за отсутствия значения в 'words' или 'перевод'")
+        #     break
+
+
     # Получение значения из нужного поля в Excel
         search_word = row['words']
         search_word_translate = row['перевод']
@@ -38,14 +55,27 @@ for index, row in df.iterrows():
 
         if exit_flag:
             saved_flag = True
+            # сделать чтобы определяло есть ли в буфере что-то
+            while True:
+                save_changes = input("Хотите ли сохранить изменения, если нажмете (+) ДА, (-) НЕТ: ") 
+                if(save_changes == '+'):
+                    saved_flag = True
+                    break
+                elif(save_changes == '-'):
+                    saved_flag = False
+                    break
+                else:
+                    print("Нету такой команды! Нажмите + или -")
+
             break  # Выход из внешнего цикла при установленном флаге
-        else: 
+        else:
             saved_flag = False
+
 
 # Сохранение обновленных данных в Excel
 if saved_flag:
 
     workbook.save(excel_file)
-    print('\033[92mСохранено\033[0m')
+    print('\033[92mИзменения сохранены\033[0m')
 else:
     print('\033[92mНичего не было сохранено\033[0m')
